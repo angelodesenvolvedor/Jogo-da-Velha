0fa2237b8172e9f5146c944a2f511f4fbb680437
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2024-02-28 08:02:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+function handleClick(index) {
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        document.getElementsByClassName('cell')[index].innerText = currentPlayer;
+        document.getElementsByClassName('cell')[index].classList.add(currentPlayer === 'X' ? 'x-piece' : 'o-piece');
+        checkWinner();
+        togglePlayer();
+    }
+}
+
+function togglePlayer() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWinner() {
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (gameBoard[a] !== '' && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            alert(`${currentPlayer} venceu!`);
+            gameActive = false;
+        }
+    }
+
+    if (!gameBoard.includes('') && gameActive) {
+        alert('Empate!');
+        gameActive = false;
+    }
+}
